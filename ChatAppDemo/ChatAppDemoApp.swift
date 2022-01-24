e57c4a667bd467372db1aedefdd1e8eda152d8fd
Austin Burke <austinlayneburke@gmail.com>
2022-01-24 06:17:06 -0600

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Austin Burke on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
